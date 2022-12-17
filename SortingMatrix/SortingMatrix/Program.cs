c19feee7a3a8e7f44e4d15ef5af2ed636578911b
Prokopenko Oleg <cotarius@yandex.ru>
2022-12-17 21:33:11 +0300

    add final functions sortByRows and sor

--- SortingMatrix/SortingMatrix/Program.cs
+++ SortingMatrix/SortingMatrix/Program.cs
@@ -35,19 +35,19 @@ namespace SortingMatrix
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.Write($"{array[i, j]} \t");
+                    Console.Write($"{array[i, j], 2}");
                 }
                 Console.WriteLine();
             }
             
         }
-        public static void SortMatrixByRows(int[,] array, int n, int m)
+        public static void SortMatrixByRows(int[,] array)
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < m - 1; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    for (int k = 0; k < m - j - 1; k++)
+                    for (int k = 0; k < array.GetLength(1) - 1; k++)
                     {
                         if (array[i, k] > array[i, k + 1])
                         {
@@ -59,27 +59,34 @@ namespace SortingMatrix
                 }
             }
         }
-            
-        static void Main(string[] args)
+        static public int[,] CreateRandomMatrix()
         {
-            int n = 4, m = 5;
-            int[,] matrix = new int[n, m];
             Random rand = new Random();
-            for (int i = 0; i < m; i++)
+            int n = rand.Next(3, 5);
+            int m = rand.Next(3, 5);
+            int[,] array = new int[n, m];
+            for(int i = 0; i < n; i++)
             {
-                for(int j = 0; j < n; j++)
+                for(int j = 0; j < m; j++)
                 {
-                    matrix[j, i] = rand.Next(1, 20);
+                    array[i, j] = rand.Next(1, 10);
                 }
                 Console.WriteLine();
             }
+            return array;
+        }
             
-
+        static void Main(string[] args)
+        {            
+            
+            var matrix = CreateRandomMatrix();
             PrintMatrix(matrix);
             Console.WriteLine();
-            SortMatrix(matrix);            
+            SortMatrixByRows(matrix);            
             PrintMatrix(matrix);
+            Console.WriteLine();
             SortMatrix(matrix);
+            PrintMatrix(matrix);
         }
     }
 }
