055302c3aaf6827a5cd0f1cdad829007038dc88a
Prokopenko Oleg <cotarius@yandex.ru>
2023-01-07 19:34:08 +0300

    testing entityFramework

--- EntityFrameworkCodeFirst(JoinTwoTables)/EntityFrameworkCodeFirst/Program.cs
+++ EntityFrameworkCodeFirst(JoinTwoTables)/EntityFrameworkCodeFirst/Program.cs
@@ -8,25 +8,113 @@ using System.Threading.Tasks;
 namespace EntityFrameworkCodeFirst
 {
     internal class Program
-    {              
+    {
+        static int stateMenu;
+                
         static void Main(string[] args)
         {
-            GetBranch();
-            Console.WriteLine();
-            GetEmployee();
-            Console.WriteLine();
-            //GetEmployeeWithBranch();
-            FullStatistic();
-            Console.ReadKey();
+            Console.Clear();
+            Menu();
+
+            while(stateMenu != 0)
+            {
+                switch(stateMenu)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            InfoMenu();
+
+                            if(stateMenu > 4 || stateMenu < 0)
+                            {
+                                return;
+                            }
+                            switch(stateMenu)
+                            {
+                                case 1:
+                                    {
+                                        Console.Clear();
+                                        GetEmployee();
+                                    }
+                                    Console.ReadKey();
+                                    Console.Clear();                                    
+                                    break;
+
+                                case 2:
+                                    {
+                                        Console.Clear();
+                                        GetBranch();
+                                    }
+                                    Console.ReadKey();
+                                    Console.Clear();                                    
+                                    break;
+
+                                case 3:
+                                    {
+                                        Console.Clear();
+                                        FullStatistic();
+                                    }
+                                    Console.ReadKey();
+                                    Console.Clear();                                    
+                                    break;
+
+                                case 4:
+                                    {
+                                        Console.Clear();
+                                        Menu();
+                                    }                                    
+                                    break;
+
+                            }
+
+                        }
+                        break;
+                }
+            }
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("**********Menu**********");
+            Console.WriteLine("1. Info");
+            Console.WriteLine("0. Exit");
+            Console.Write("Input number: ");
+            while(!Int32.TryParse(Console.ReadLine(), out stateMenu))
+            {
+                Console.Write("Incorrect number!");
+            }
+            if(stateMenu > 1 || stateMenu < 0)
+            {
+                Console.WriteLine("Incorrect number!");
+                return;
+            }
+        }
+        public static void InfoMenu()
+        {
+            Console.WriteLine("********Info Menu********");
+            Console.WriteLine("1. List of Employees");
+            Console.WriteLine("2. List of Branches");
+            Console.WriteLine("3. Full List");
+            Console.WriteLine("4. Back");
+            Console.Write("Enter your choice: ");
+
+            while (!Int32.TryParse(Console.ReadLine(), out stateMenu))
+            {
+                Console.WriteLine("Incorrect number!");
+            }
         }
         public static void GetEmployee()
         {
             using (Bank db = new Bank())
             {
+                Console.WriteLine("{0, -5}{1, -15}{2,-15}{3,-15}{4,-15}", "Id", "First Name", 
+                    "Last Name", "Start Date", "Title");
+                Console.WriteLine("----------------------------------------------------------" +
+                    "-----------------");
                 foreach (EMPLOYEE empl in db.EMPLOYEE)
                 {
-                    Console.WriteLine($"{empl.EMP_ID,-5}{empl.FIRST_NAME,-15}{empl.LAST_NAME,-15}{empl.START_DATE.ToShortDateString(),-15}" +
-                        $"{empl.TITLE,-15}");
+                    Console.WriteLine($"{empl.EMP_ID,-5}{empl.FIRST_NAME,-15}{empl.LAST_NAME,-15}" +
+                        $"{empl.START_DATE.ToShortDateString(),-15}{empl.TITLE,-15}");
                 }
             }
         }
@@ -34,6 +122,8 @@ namespace EntityFrameworkCodeFirst
         {
             using (Bank db = new Bank())
             {
+                Console.WriteLine("{0,-5}{1,-25}{2,-15}{3,-15}{4,-15}{5,-10}", "Id","Address","City","Name", "State", "Zip Code");
+                Console.WriteLine("-------------------------------------------------------------------------------------");
                 foreach (BRANCH br in db.BRANCH)
                 {
                     Console.WriteLine($"{br.BRANCH_ID,-5}{br.ADDRESS,-25}{br.CITY,-15}{br.NAME,-15}" +
@@ -79,7 +169,10 @@ namespace EntityFrameworkCodeFirst
                         BranchName = ee.b.NAME,
                         BranchCity = ee.b.CITY
                     });
-
+                Console.WriteLine("{0,-15}{1,-15}{2,-22}{3,-17}{4,-15}{5,-15}", "First Name", "Last Name",
+                    "Title", "Department", "Branch", "City");
+                Console.WriteLine("------------------------------------------------------------------------" +
+                    "---------------------");
                 foreach (var l in list)
                 {
                     Console.WriteLine($"{l.EmpFirstName,-15}{l.EmpLastName,-15}{l.EmpTitle,-22}{l.Dept,-17}" +
