108fac8d213234ff1302d35edfa025d1faa79fed
Oleg Prokopenko <cotarius@yandex.ru>
2023-03-08 17:51:28 +0300

    Add Quiz(csv)

---
+++ QUIZ(csv)/QUIZ/DataBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ
+{
+    public class DataBase
+    {
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+
+        public DataBase(string question, string answer) 
+        {
+            Question = question;
+            Answer = answer;
+        }
+        public DataBase() { }
+
+        public override string ToString()
+        {
+            return $"{Question} - {Answer}";
+        }
+    }
+}
